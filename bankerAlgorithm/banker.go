cc035431a26e5042992f7e4df2e70b066f0e3218
arronsma <arronsma@tencent.com>
2022-06-12 23:14:59 -0700

    safe judge

--- bankerAlgorithm/banker.go
+++ bankerAlgorithm/banker.go
@@ -1,10 +1,68 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"strconv"
+)
 
-func minusMatrix(m1[][] int, m2[][] int)  {
-	
+// if all element in v1 is less or equal than v2
+func vecAllLe(v1, v2 [] int) (bool) { 
+	for i := range(v1){
+		if (v1[i] > v2[i]){
+			return false
+		}
+	}
+	return true
 }
+
+func vecAdd(v1, v2 [] int, v3 *[] int){
+	for i := range(v1){
+		(*v3)[i] = v1[i] + v2[i]
+	}
+}
+
+func isSafe(sourceVec [] int, max, allocation, need[][] int, process, source int)(bool)  {
+	finish := make([] bool, 5)
+	res := process 
+	work := make([] int, source)
+	copy(work, sourceVec)
+	var order [] int
+	for i := range(finish){
+		finish[i] = false
+	}
+
+	for res != 0{
+		getOne := false
+		for i := range(finish){
+			if (finish[i] == true){
+				continue
+			}
+
+			if (vecAllLe(need[i], work)){
+				order = append(order, i)
+				finish[i] = true
+				vecAdd(work, allocation[i], &work)
+				res -= 1
+				getOne = true
+			}
+		}
+
+		if (getOne == false){
+			return false
+		}
+	}
+
+	return true
+}
+
+func vectorRead(vec* [] int, l int){
+	for i := 0; i < l; i++{
+		var t int
+		fmt.Scan(&t)
+		(*vec) = append((*vec), t)
+	}
+}
+
 
 func matrixRead(matrix* [][] int, r,c int)  {
 	for i:=0 ; i < r; i++ {
@@ -28,11 +86,20 @@ func matrixMinus(m1 [][] int, m2[][] int, res* [][] int, m, n int){
 	}
 }
 
+
 func main()  {
+
+
+
 	var process, source int
 	fmt.Printf("please enter process number and source number\n")
 	fmt.Scan(&process, &source)
 	fmt.Printf("there are %d process and %d source\n", process, source)
+
+	fmt.Printf("please enter source vector")
+	var sourceVector [] int
+	vectorRead(&sourceVector, source)
+	fmt.Println(sourceVector)
 
 	fmt.Printf("please enter max matrix:\n")
 	var max[][] int
@@ -47,6 +114,8 @@ func main()  {
 	var need[][] int
 	matrixMinus(max, allocation, &need, process, source)
 	fmt.Println(need)
+
+	fmt.Printf("current status is safe? %s\n", strconv.FormatBool(isSafe(sourceVector, max, allocation, need,process, source)))
 
 	
 
